ece20b4672fdfd39c2cba9b7c6357d306a4b7f2f
Billy Hardy <billy92@gmail.com>
2021-07-28 15:00:25 +0000

    Add scripts to move firebase files around

---
+++ update-from-day11.sh
@@ -0,0 +1,3 @@
+cp ~/cssi-day11/starter/firebaseNotes/firebase.json ~/cssi-day12/starter/firebaseNotes/firebase.json
+cp ~/cssi-day11/starter/firebaseNotes/database.rules.json ~/cssi-day12/starter/firebaseNotes/database.rules.json
+cp ~/cssi-day11/starter/firebaseNotes/.firebaserc ~/cssi-day12/starter/firebaseNotes/.firebaserc
---
+++ update-to-final.sh
@@ -0,0 +1,3 @@
+cp starter/firebaseNotes/firebase.json final/firebaseNotes/firebase.json
+cp starter/firebaseNotes/database.rules.json final/firebaseNotes/database.rules.json
+cp starter/firebaseNotes/.firebaserc final/firebaseNotes/.firebaserc
